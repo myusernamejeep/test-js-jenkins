49e71a34e5920d6bd02a224c3ffe1cd31664babe
Jumphol Tangtrairattanakul <jumphol@crie.co.th>
2013-08-19 17:15:52 +0700

    เพิ่ม test case

--- test/test_spec.js
+++ test/test_spec.js
@@ -65,3 +65,27 @@ describe("Test TV Volume", function(){
         expect(myTv.volume()).toEqual(100);
     });
 });
+
+
+describe("#Test New ", function(){
+    var myTv = null;
+
+    beforeEach(function() {
+        myTv = Tv();
+    });
+
+    it("try changing the volume while off", function(){
+        expect(myTv.setVolume(100)).toEqual(false);
+    });
+
+    it("check volume while off", function(){
+        expect(myTv.volume()).toEqual(false);
+    });
+
+    it("turn the tv on, than change the volume", function(){
+        expect(myTv.turnOn()).toEqual(true);
+        expect(myTv.volume()).toEqual(50);
+        expect(myTv.setVolume(100)).toEqual(true);
+        expect(myTv.volume()).toEqual(100);
+    });
+});
